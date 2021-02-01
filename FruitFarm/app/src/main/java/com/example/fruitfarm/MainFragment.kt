2a83dc33a08fbf1251c2ec87b6c10b2fe7debc3c
Israel Hilerio <israelhilerio@Israels-MacBook-Air.local>
2021-01-31 19:12:34 -0800

    first commit

---
+++ FruitFarm/app/src/main/java/com/example/fruitfarm/MainFragment.kt
@@ -0,0 +1,56 @@
+package com.example.fruitfarm
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val FRUIT_COUNT = "fruitCount"
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [MainFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class MainFragment : Fragment() {
+    // TODO: Rename and change types of parameters
+    private var fruitCount: Int? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+            fruitCount = it.getInt(FRUIT_COUNT)
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_main, container, false)
+    }
+
+    companion object {
+        /**
+         * Use this factory method to create a new instance of
+         * this fragment using the provided parameters.
+         *
+         * @param param1 Parameter 1.
+         * @param param2 Parameter 2.
+         * @return A new instance of fragment MainFragment.
+         */
+        // TODO: Rename and change types and number of parameters
+        @JvmStatic
+        fun newInstance(fruitCount: Int) =
+            MainFragment().apply {
+                arguments = Bundle().apply {
+                    // TODO: maybe remove this
+                    putInt(FRUIT_COUNT, fruitCount)
+                }
+            }
+    }
+}
